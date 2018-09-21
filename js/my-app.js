ce6d00508ddd0107b26456fe3c15346a77b4bc62
marcoalejandrooo <37090921+marcoalejandrooo@users.noreply.github.com>
2018-09-20 18:03:49 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,65 @@
+// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'md', //ios, md, auto
+    modalTitle: 'Framework7', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/producto/',
+    	url: 'producto.html',
+    	name: 'producto',
+  		},
+		{
+		path: '/about/',
+    	url: 'about.html',
+    	name: 'about',
+  		},
+		{
+		path: '/shops/',
+    	url: 'shops.html',
+    	name: 'shops',
+  		},
+		{
+		path: '/shops-tgu/',
+    	url: 'shops-tgu.html',
+    	name: 'shops-tgu',
+  		},
+		{
+		path: '/shops-sps/',
+    	url: 'shops-sps.html',
+    	name: 'shops-sps',
+  		},
+		{
+		path: '/arbol/',
+    	url: 'arbol.html',
+    	name: 'arbol',
+  		},
+		{
+		path: '/habitacion/',
+    	url: 'habitacion.html',
+    	name: 'habitacion',
+  		}
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
